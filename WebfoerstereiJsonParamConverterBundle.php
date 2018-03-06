e35161d5b40f6d265940b95e2161426b72da72df
Timo Förster <tfoerster@webfoersterei.de>
2018-03-06 18:56:50 +0100

    Basic bundle structure

---
+++ WebfoerstereiJsonParamConverterBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Timo Förster <tfoerster@webfoersterei.de>
+ * @date 06.03.18
+ */
+
+namespace Webfoersterei\Bundle\JsonParamConverterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebfoerstereiJsonParamConverterBundle extends Bundle
+{
+
+}
