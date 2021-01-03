e3d0076add1acd47f1468c4dd052b40e4e85a5a6
Timo Förster <tfoerster@webfoersterei.de>
2021-01-03 01:09:08 +0100

    Add annotation to identify valid JsonDTOs Add unitests

---
+++ ParamConverter/JsonDto.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Timo Förster <tfoerster@webfoersterei.de>
+ * @date 02.01.21
+ */
+
+namespace Webfoersterei\Bundle\JsonParamConverterBundle\ParamConverter;
+
+#[\Attribute]
+class JsonDto
+{
+    public function __construct()
+    {
+    }
+}
